ab8a8f2a4b3338d8f19679d2cf684be30315255b
FireVentus <petergao96@yahoo.ca>
2020-03-06 09:54:03 -0500

    made splash page

--- app/src/main/java/com/example/recyclereducerecipe/MainActivity.java
+++ app/src/main/java/com/example/recyclereducerecipe/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.recyclereducerecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+    View createRecipe;
+    View selectRecipe;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //createRecipe = findViewById(R.id.create);
+        //selectRecipe = findViewById(R.id.select);
+
     }
+
+    public void CreateThisRecipe(View view){
+        //TODO: go to creation view
+    }
+
+    public void GoToRecipeList(View view){
+        // TODO: go to recipelist view
+    }
+
+
+
 }
